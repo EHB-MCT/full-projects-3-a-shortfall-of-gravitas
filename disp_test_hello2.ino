a65cc73a9469055f2781b40e5785f23a180115f4
Rhys Devalckeneer <44397726+RhysDevalckeneer2@users.noreply.github.com>
2022-01-20 20:51:41 +0100

    Added Arduino code

---
+++ disp_test_hello2.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(200);
+  display.clearDisplay();
+
+  display.setTextSize(6);
+  display.setTextColor(WHITE);
+  
+}
+
+void loop() {
+  display.setCursor(15, 10);
+  display.println("Yo");
+  display.display(); 
+  delay(2000);
+  
+  display.clearDisplay();
+  display.setCursor(15, 10);
+  display.println("Hey");
+  display.display(); 
+  delay(2000);
+  
+  display.clearDisplay();
+  display.setCursor(15, 10);
+  display.println("Boo");
+  display.display();
+  delay(2000); 
+
+  display.clearDisplay();
+  display.setCursor(15, 10);
+  display.println("Oke");
+  display.display(); 
+  delay(2000);
+}
